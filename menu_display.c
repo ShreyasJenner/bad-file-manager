4a7f5bdc8393c5549eca80f711a289fd13dcd173
ShreyasJenner <shreyasjenner4@gmail.com>
2024-01-22 12:01:18 +0530

    first commit to branch

--- menu_display.c
+++ menu_display.c
@@ -2,26 +2,19 @@
 #include <stdio.h>
 #include <menu.h>
 #include <curses.h>
-#include <panel.h>
 #include <stdlib.h>
 // STANDARD HEADER FILES //
 
 // HEADER FILES //
 #include "menu_display.h"
 #include "print_title.h"
-#include "render.h"
-#include "menu_destruction.h"
-#include "window_destruction.h"
-#include "set_menu.h"
-#include "window_creation.h"
 // HEADER FILES //
 
 void menu_display(int argc, char *argv[]) {
     // Declaration //
     ITEM **my_items;
     MENU *my_menu;
-    WINDOW *my_menu_win[3], *title_win;
-    PANEL *my_panels[3];
+    WINDOW *my_menu_win, *title_win;
     
     char store[argc-1][4];
     int c,i,cur_win_index;
@@ -53,36 +46,41 @@ void menu_display(int argc, char *argv[]) {
         sprintf(store[i-1],"%d",i);
         my_items[i-1] = new_item(store[i-1], argv[i]);
     }
-    my_items[argc] = (ITEM *)NULL;
     
     my_menu = new_menu((ITEM**)my_items);
     // Create Menu Items and Menu //
 
     
-    // Create windows and panels //
+    // Create windows //
     getmaxyx(stdscr, nlines, ncols);
-    window_creation(ncols, nlines, startx, starty, &title_win, my_menu_win);
-    for(i=0;i<3;i++)
-        my_panels[i] = new_panel(my_menu_win[i]);
-    // Create windows and panels //
+
+    my_menu_win = newwin(10, 20, starty+3, startx);
+    title_win = newwin(3, ncols, starty, startx);
+
+    // Create windows //
 
 
     // sets menu main and sub windows ; sets menu mark//
-    set_menu(my_menu, my_menu_win[0],ncols);
+    menu_opts_off(my_menu, O_NONCYCLIC);
+    set_menu_win(my_menu, my_menu_win);
+    set_menu_sub(my_menu, derwin(my_menu_win,6,20,1,1));
+
+    set_menu_mark(my_menu, "*");
     // sets menu main and sub windows ; sets menu mark//
      
   
     // Print border around main window; add title//
     print_title(title_win, 1, startx, ncols, "File Manager", COLOR_PAIR(1));
-    mvwprintw(title_win, 0, 0, "%d %d", nlines, ncols);
-    
-    for(i=0;i<3;i++)
-        box(my_menu_win[i], 0, 0);
+   
+    box(my_menu_win, 0, 0);
     // Print border around main window and title //
    
     
     // Render //
-    render(title_win, my_panels, my_menu);
+    wrefresh(title_win);
+
+    post_menu(my_menu);
+    wrefresh(my_menu_win);
     // Render //
 
 
@@ -90,40 +88,20 @@ void menu_display(int argc, char *argv[]) {
     while((c=getch()) != 'q') {
         ITEM *cur_item = current_item(my_menu);
         switch(c) {
-            case KEY_RESIZE:
-            {
-                WINDOW *old_title = title_win;
-                WINDOW **old_menu_win = my_menu_win;
-                getmaxyx(stdscr, ncols, nlines);
-                window_creation(ncols, nlines, startx, starty, &title_win, my_menu_win);
-                for(i=0;i<3;i++)
-                    replace_panel(my_panels[i],my_menu_win[i]);
-                render(title_win, my_panels, my_menu);
-                window_destruction(old_menu_win, old_title);
-                break;
-            }
-
             case 'j':
-                if(cur_item == my_items[argc-2])
-                    menu_driver(my_menu, REQ_FIRST_ITEM);
-                else
-                    menu_driver(my_menu, REQ_DOWN_ITEM);
+                menu_driver(my_menu, REQ_DOWN_ITEM);
                 break;
 
             case 'k':
-                if(cur_item == my_items[0])
-                    menu_driver(my_menu, REQ_LAST_ITEM);
-                else
-                    menu_driver(my_menu, REQ_UP_ITEM);
+                menu_driver(my_menu, REQ_UP_ITEM);
                 break;
 
             case 'l':
-                cur_win_index++;
-                cur_win_index %= 3;
+                menu_driver(my_menu, REQ_RIGHT_ITEM);
                 break;
 
             case 'h':
-                cur_win_index = (cur_win_index-1+3)%3;
+                menu_driver(my_menu, REQ_LEFT_ITEM);
                 break;
             
             case KEY_NPAGE:
@@ -134,14 +112,17 @@ void menu_display(int argc, char *argv[]) {
                 menu_driver(my_menu, REQ_SCR_UPAGE);
                 break;
         }
-        wrefresh(my_menu_win[0]);
+        wrefresh(my_menu_win);
     }
     /* Main Loop */
    
     
     // Free memory //
-    menu_destruction(argc, my_menu, my_items);
-    window_destruction(my_menu_win,title_win);
+    unpost_menu(my_menu);
+    free_menu(my_menu);
+    for(i=0;i<argc;i++)
+        free_item(my_items[i]);
+
     // Free memory //
 
     endwin();
--- window_destruction.h
+++
@@ -1,8 +0,0 @@
-#ifndef WINDOW_DESTRUCTION_H 
-#define WINDOW_DESTRUCTION_H
-
-#include <curses.h>
-#include <menu.h>
-
-void window_destruction(WINDOW **my_menu_win, WINDOW *title_win);
-#endif
